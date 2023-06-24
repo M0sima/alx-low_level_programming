52a6e78b4df03af07794259ead91caf1eb78dcd4
M0sima <mosimam3@gmail.com>
2023-06-24 05:48:04 -0700

    3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_numbers - Print the numbers since 0 up to 9
+ * Return: 0
+ */
+
+void print_numbers(void)
+
+{
+
+	char c;
+
+	for (c = 0; c <= 9; c++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
+}
