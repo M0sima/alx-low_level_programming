a00ab88b1f715ef810819691caea8828daad096c
M0sima <mosimam3@gmail.com>
2023-06-27 17:48:45 -0700

    4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* print_rev - prints a string in reverse
+* @x: the string
+* Return : void
+*/
+
+void print_rev(char *x)
+
+{
+	int c = 0;
+
+	while (x[c] != '\0')
+	{
+	c++;
+	for (x -= 1; c >= 0; c--)
+	_putchar(x[c]);
+	}
+	_putchar('\n');
+}
